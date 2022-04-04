70a0c1ff54fa49e7bb547ab23a96ce748f160be6
fmonteil2021 <fmonteil2021@53SE13-D1XF703.ad.campus-eni.fr>
2022-04-04 12:03:18 +0200

    ajout encheres

---
+++ src/main/java/fr/eni/encheres/bo/Encheres.java
@@ -0,0 +1,10 @@
+package fr.eni.encheres.bo;
+
+import java.time.LocalDate;
+
+public class Encheres {
+	private LocalDate date_enchere;
+	private Integer montant_enchere;
+	private Utilisateurs utilisateur;
+		
+}
