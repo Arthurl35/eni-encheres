d782b3c7552d66a1fd04d6276bc2165c211b10f8
fmonteil2021 <fmonteil2021@53SE13-D1XF703.ad.campus-eni.fr>
2022-04-04 16:38:35 +0200

    ajout contrainte d'insertion d'utilisateurs (email et num de telephone)

--- src/main/java/fr/eni/encheres/bll/UtilisateursManager.java
+++ src/main/java/fr/eni/encheres/bll/UtilisateursManager.java
@@ -5,5 +5,7 @@ import java.util.List;
 import fr.eni.encheres.bo.Utilisateurs;
 
 public interface UtilisateursManager {
+	
+	public void addUtilisateurs(Utilisateurs utilisateur) throws UtilisateursException;
 	public List<Utilisateurs> getAllUtilisateurs() throws UtilisateursException;
 }
--- src/main/java/fr/eni/encheres/bll/UtilisateursManagerImpl.java
+++ src/main/java/fr/eni/encheres/bll/UtilisateursManagerImpl.java
@@ -5,7 +5,26 @@ import java.util.List;
 import fr.eni.encheres.bo.Utilisateurs;
 
 public class UtilisateursManagerImpl implements UtilisateursManager {
-	
+
+	@Override
+	public void addUtilisateurs(Utilisateurs utilisateur) throws UtilisateursException {
+		try {
+			for (Utilisateurs u : dao.getAll()) {
+				if (utilisateur.getEmail().equals(u.getEmail())) {
+					throw new UtilisateursException("Email déjà existant !");
+				}
+				if (utilisateur.getTelephone().equals(u.getTelephone())) {
+					throw new UtilisateursException("Numéro de téléphone déjà existant !");
+				}
+			}
+
+			dao.insert(utilisateur);
+		} catch (DALException e) {
+			throw new UtilisateursException("Problème à l'insertion");
+		}
+
+	}
+
 	@Override
 	public List<Utilisateurs> getAllUtilisateurs() throws UtilisateursException {
 		try {
@@ -14,4 +33,5 @@ public class UtilisateursManagerImpl implements UtilisateursManager {
 			throw new UtilisateursException("Problème à la selection");
 		}
 	}
+
 }
