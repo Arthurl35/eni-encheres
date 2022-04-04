e4f789e24c69f3ee5f6dcc5ba57c9a36c38b3211
mgoovaerts2021 <mgoovaerts2021@53SE13-H0XF703.ad.campus-eni.fr>
2022-04-04 12:05:18 +0200

    create Class ArticleVendu

---
+++ src/main/java/fr/eni/encheres/bo/ArticleVendu.java
@@ -0,0 +1,102 @@
+package fr.eni.encheres.bo;
+
+import java.time.LocalDate;
+
+public class ArticleVendu {
+	
+	private Integer noArticle;
+	private String nomArticle;
+	private String description;
+	private LocalDate dateDebutEncheres;
+	private LocalDate dateFinEncheres;
+	private Integer miseAPrix;
+	private Integer prixVente;
+	private Integer etatVente;
+	
+	public ArticleVendu() {}
+
+	public ArticleVendu(Integer noArticle, String nomArticle, String description, LocalDate dateDebutEncheres,
+			LocalDate dateFinEncheres, Integer miseAPrix, Integer prixVente, Integer etatVente) {
+		super();
+		this.noArticle = noArticle;
+		this.nomArticle = nomArticle;
+		this.description = description;
+		this.dateDebutEncheres = dateDebutEncheres;
+		this.dateFinEncheres = dateFinEncheres;
+		this.miseAPrix = miseAPrix;
+		this.prixVente = prixVente;
+		this.etatVente = etatVente;
+	}
+
+	public Integer getNoArticle() {
+		return noArticle;
+	}
+
+	public void setNoArticle(Integer noArticle) {
+		this.noArticle = noArticle;
+	}
+
+	public String getNomArticle() {
+		return nomArticle;
+	}
+
+	public void setNomArticle(String nomArticle) {
+		this.nomArticle = nomArticle;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public LocalDate getDateDebutEncheres() {
+		return dateDebutEncheres;
+	}
+
+	public void setDateDebutEncheres(LocalDate dateDebutEncheres) {
+		this.dateDebutEncheres = dateDebutEncheres;
+	}
+
+	public LocalDate getDateFinEncheres() {
+		return dateFinEncheres;
+	}
+
+	public void setDateFinEncheres(LocalDate dateFinEncheres) {
+		this.dateFinEncheres = dateFinEncheres;
+	}
+
+	public Integer getMiseAPrix() {
+		return miseAPrix;
+	}
+
+	public void setMiseAPrix(Integer miseAPrix) {
+		this.miseAPrix = miseAPrix;
+	}
+
+	public Integer getPrixVente() {
+		return prixVente;
+	}
+
+	public void setPrixVente(Integer prixVente) {
+		this.prixVente = prixVente;
+	}
+
+	public Integer getEtatVente() {
+		return etatVente;
+	}
+
+	public void setEtatVente(Integer etatVente) {
+		this.etatVente = etatVente;
+	}
+
+	@Override
+	public String toString() {
+		return "ArticleVendu [noArticle=" + noArticle + ", nomArticle=" + nomArticle + ", description=" + description
+				+ ", dateDebutEncheres=" + dateDebutEncheres + ", dateFinEncheres=" + dateFinEncheres + ", miseAPrix="
+				+ miseAPrix + ", prixVente=" + prixVente + ", etatVente=" + etatVente + "]";
+	}
+	
+}
