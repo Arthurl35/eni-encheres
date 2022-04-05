91ef05d7ecc4e36356d0423f314a4393eefd58ea
fmonteil2021 <fmonteil2021@53SE13-D1XF703.ad.campus-eni.fr>
2022-04-05 10:33:02 +0200

    Correction

--- src/main/java/fr/eni/encheres/dal/DAOFact.java
+++ src/main/java/fr/eni/encheres/dal/DAOFact.java
@@ -5,7 +5,7 @@ import fr.eni.encheres.dal.utilisateur.UtilisateurDAO;
 import fr.eni.encheres.dal.utilisateur.UtilisateurDAOImpl;
 
 public class DAOFact {
-	public static UtilisateurDAO getUtilisateurtDAO() {
+	public static UtilisateurDAO getUtilisateurDAO() {
 		return new UtilisateurDAOImpl();
 	}
 }	
